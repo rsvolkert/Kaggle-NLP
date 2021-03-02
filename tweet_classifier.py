7ba1590320317ae6724a33ebf2f5f25f319cd3cd
Ryan Volkert <57147542+rsvolkert@users.noreply.github.com>
2021-03-02 10:25:29 -0700

    Made it better for people who are not me

--- tweet_classifier.py
+++ tweet_classifier.py
@@ -30,62 +30,75 @@ def clean(text):
     
     return clean.lower()
 
-# helpful functions
-def fit(model, method):
-    if method == 'tfidf':
-        train = train_tf_features
-        test = test_tf_features
-    else:
-        train = train_bow_features
-        test = test_bow_features
-    
-    return model.fit(train, train2.target).predict(test)
+train['clean_text'] = train['text'].apply(lambda x : clean(x))
 
-def append_df(model_name, method, model):
-    predictions = fit(model, method)
+# helpful functions
+def append_df(model_name, method, predictions):
     return acc.append({'model' : model_name,
                        'method' : method,
                        'accuracy' : accuracy_score(test2['target'], predictions)},
                       ignore_index = True)
 
-train['clean_text'] = train['text'].apply(lambda x : clean(x))
 
 train2 = train.sample(frac=.6, random_state=400)
 test2 = train.drop(train2.index)
 
-## BoW
-bow = CountVectorizer()
-
-train_bow_features = bow.fit_transform(train2['clean_text'])
-test_bow_features = bow.transform(test2['clean_text'])
-
 ## TfIdf
 tfidf = TfidfVectorizer(ngram_range = (1,2))
 
-train_tf_features = tfidf.fit_transform(train2['clean_text'])
-test_tf_features = tfidf.transform(test2['clean_text'])
+train_tf = tfidf.fit_transform(train2['clean_text'])
+test_tf = tfidf.transform(test2['clean_text'])
 
 # svm
-acc = append_df('SVM', 'tfidf', LinearSVC(random_state=400))
+svm_tfidf = LinearSVC(random_state=400)
+svm_tfidf_fit = svm_tfidf.fit(train_tf, train2.target)
+svm_tfidf_pred = svm_tfidf_fit.predict(test_tf)
+acc = append_df('SVM', 'tfidf', svm_tfidf_pred)
 
 # logistic regression
-acc = append_df('Logistic Regression', 'tfidf', LogisticRegression(random_state=400))
+log_tfidf = LogisticRegression(random_state=400)
+log_tfidf_fit = log_tfidf.fit(train_tf, train2.target)
+log_tfidf_pred = log_tfidf_fit.predict(test_tf)
+acc = append_df('Logistic Regression', 'tfidf', log_tfidf_pred)
 
 # random forest
-acc = append_df('Random Forest', 'tfidf', RandomForestClassifier(random_state=400))
+rf_tfidf = RandomForestClassifier(random_state=400)
+rf_tfidf_fit = rf_tfidf.fit(train_tf, train2.target)
+rf_tfidf_pred = rf_tfidf_fit.predict(test_tf)
+acc = append_df('Random Forest', 'tfidf', rf_tfidf_pred)
 
 # naive bayes
-acc = append_df('Naive Bayes', 'tfidf', MultinomialNB())
+nb_tfidf = MultinomialNB()
+nb_tfidf_fit = nb_tfidf.fit(train_tf, train2.target)
+nb_tfidf_pred = nb_tfidf_fit.predict(test_tf)
+acc = append_df('Naive Bayes', 'tfidf', nb_tfidf_pred)
 
+## BoW
+bow = CountVectorizer()
+
+train_bow = bow.fit_transform(train2['clean_text'])
+test_bow = bow.transform(test2['clean_text'])
 
 # svm
-acc = append_df('SVM', 'bow', LinearSVC(random_state=400))
+svm_bow = LinearSVC(random_state=400)
+svm_bow_fit = svm_bow.fit(train_bow, train2.target)
+svm_bow_pred = svm_bow_fit.predict(test_bow)
+acc = append_df('SVM', 'bow', svm_bow_pred)
 
 # logistic regression
-acc = append_df('Logistic Regression', 'bow', LogisticRegression(random_state=400))
+log_bow = LogisticRegression(random_state=400)
+log_bow_fit = log_bow.fit(train_bow, train2.target)
+log_bow_pred = log_bow_fit.predict(test_bow)
+acc = append_df('Logistic Regression', 'bow', log_bow_pred)
 
 # random forest
-acc = append_df('Random Forest', 'bow', RandomForestClassifier(random_state=400))
+rf_bow = RandomForestClassifier(random_state=400)
+rf_bow_fit = rf_bow.fit(train_bow, train2.target)
+rf_bow_pred = rf_bow_fit.predict(test_bow)
+acc = append_df('Random Forest', 'bow', rf_bow_pred)
 
 # naive bayes
-acc = append_df('Naive Bayes', 'bow', MultinomialNB())
+nb_bow = MultinomialNB()
+nb_bow_fit = nb_bow.fit(train_bow, train2.target)
+nb_bow_pred = nb_bow_fit.predict(test_bow)
+acc = append_df('Naive Bayes', 'bow', nb_bow_pred)
