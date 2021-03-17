d2a72bad3f2cfdc99f8a9f867c13687fa36e1369
Dawson <dawsoneliasen@gmail.com>
2021-03-17 14:55:42 -0600

    create submission script

---
+++ submission.py
@@ -0,0 +1,54 @@
+import re
+import string
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+train = pd.read_csv('Data/train.csv')
+
+
+def clean(text):
+    nourl = re.sub(r'[\S|\s]http\S+', '', text)
+    noat = re.sub(r'\s@\S+', '', nourl)
+    tokens = word_tokenize(noat)
+    lemmatizer = WordNetLemmatizer()
+    lemma = []
+    for word in tokens:
+        if word not in stopwords.words('english'):
+            lemma.append(lemmatizer.lemmatize(word))
+    lemma = ' '.join([word for word in lemma])
+    nopunct = "".join([char for char in lemma if char not in string.punctuation])
+    clean_text = re.sub(' +', ' ', nopunct)
+    return clean_text.lower()
+
+
+train['clean_text'] = train['text'].apply(lambda x: clean(x))
+
+# TF-IDF
+tfidf = TfidfVectorizer(ngram_range=(1, 2))
+
+train_tf = tfidf.fit_transform(train.clean_text)
+
+# grid CV
+grid = {'penalty': ['l1', 'l2', 'elasticnet', 'none'],
+        'dual': [True, False],
+        'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+        'C': [100, 10, 1, 0.1, 0.01]}
+
+model = LogisticRegression(random_state=400)
+model.fit(train_tf, train['target'].values)
+test = pd.read_csv('Data/test.csv')
+test['clean_text'] = test['text'].apply(lambda x: clean(x))
+test_tf = tfidf.transform(test['clean_text'])
+ypred = model.predict(test_tf)
+test['target'] = ypred
+submission = test.drop(['keyword', 'location', 'text', 'clean_text'], axis=1)
+submission.to_csv('Data/submission.csv', index=False)
